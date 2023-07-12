f9d9306556d35ce001afd5c2d6ad7433b7e78ba9
ilyas <ilyas@ILYAS-HADI>
2023-07-12 13:46:08 +0500

    first commit

---
+++ assets/js/mapdata.js
@@ -0,0 +1,516 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+    //General settings
+		width: "responsive", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "no",
+    border_color: "#ffffff",
+    popups: "on_hover",
+    
+		//State defaults
+		state_description: "",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+		//Location defaults
+		/* location_description: "Store closest to you!",
+    location_color: "#FF0067",
+    location_opacity: 1,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no", */
+    
+		//Label defaults
+		label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+   
+		//Zoom settings
+		manual_zoom: "no",
+    back_image: "no",
+    arrow_color: "#cecece",
+    arrow_color_border: "#808080",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"121"
+
+    },
+    AK: {
+      name: "Alaska",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"122"
+    },
+    FL: {
+      name: "Florida",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      inactive: "no",
+      id:"123"
+    },
+    NH: {
+      name: "New Hampshire",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"124"
+    },
+    VT: {
+      name: "Vermont",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"125"
+    },
+    ME: {
+      name: "Maine",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"126"
+    },
+    RI: {
+      name: "Rhode Island",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"127"
+    },
+    NY: {
+      name: "New York",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"128"
+    },
+    PA: {
+      name: "Pennsylvania",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"129"
+    },
+    NJ: {
+      name: "New Jersey",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"130"
+    },
+    DE: {
+      name: "Delaware",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"131"
+    },
+    MD: {
+      name: "Maryland",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"132"
+    },
+    VA: {
+      name: "Virginia",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"133"
+    },
+    WV: {
+      name: "West Virginia",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"134"
+    },
+    OH: {
+      name: "Ohio",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"135"
+    },
+    IN: {
+      name: "Indiana",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      id:"136"
+    },
+    IL: {
+      name: "Illinois",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default",
+      id:"136"
+    },
+    CT: {
+      name: "Connecticut",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default"
+    },
+    WI: {
+      name: "Wisconsin",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    NC: {
+      name: "North Carolina",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default"
+    },
+    DC: {
+      name: "District of Columbia",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default"
+    },
+    MA: {
+      name: "Massachusetts",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default"
+    },
+    TN: {
+      name: "Tennessee",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    AR: {
+      name: "Arkansas",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    MO: {
+      name: "Missouri",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    GA: {
+      name: "Georgia",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default"
+    },
+    SC: {
+      name: "South Carolina",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default"
+    },
+    KY: {
+      name: "Kentucky",
+      description: "default",
+      color: "#feea71",
+      zoomable: "no",
+      hover_color: "default",
+      url: "default"
+    },
+    AL: {
+      name: "Alabama",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    LA: {
+      name: "Louisiana",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    MS: {
+      name: "Mississippi",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    IA: {
+      name: "Iowa",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    MN: {
+      name: "Minnesota",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    OK: {
+      name: "Oklahoma",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    TX: {
+      name: "Texas",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    NM: {
+      name: "New Mexico",
+      description: "default",
+      color: "#c7cb63",
+      hover_color: "default",
+      url: "default"
+    },
+    KS: {
+      name: "Kansas",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    NE: {
+      name: "Nebraska",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    SD: {
+      name: "South Dakota",
+      description: "default",
+      color: "#c7cb63",
+      hover_color: "default",
+      url: "default"
+    },
+    ND: {
+      name: "North Dakota",
+      description: "default",
+      color: "#e7a65f",
+      hover_color: "default",
+      url: "default"
+    },
+    WY: {
+      name: "Wyoming",
+      description: "default",
+      color: "#c7cb63",
+      hover_color: "default",
+      url: "default"
+    },
+    MT: {
+      name: "Montana",
+      description: "default",
+      color: "#c7cb63",
+      hover_color: "default",
+      url: "default"
+    },
+    CO: {
+      name: "Colorado",
+      description: "default",
+      color: "#c7cb63",
+      hover_color: "default",
+      url: "default"
+    },
+    UT: {
+      name: "Utah",
+      description: "default",
+      color: "#c7cb63",
+      hover_color: "default",
+      url: "default"
+    },
+    AZ: {
+      name: "Arizona",
+      description: "default",
+      color: "#c7cb63",
+      hover_color: "default",
+      url: "default"
+    },
+    NV: {
+      name: "Nevada",
+      description: "default",
+      color: "#b1b4cb",
+      hover_color: "default",
+      url: "default"
+    },
+    OR: {
+      name: "Oregon",
+      description: "default",
+      color: "#b1b4cb",
+      hover_color: "default",
+      url: "default"
+    },
+    WA: {
+      name: "Washington",
+      description: "default",
+      color: "#b1b4cb",
+      hover_color: "default",
+      url: "default"
+    },
+    CA: {
+      name: "California",
+      description: "default",
+      color: "#b1b4cb",
+      hover_color: "default",
+      url: "default"
+    },
+    MI: {
+      name: "Michigan",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default"
+    },
+    ID: {
+      name: "Idaho",
+      description: "default",
+      color: "#c7cb63",
+      hover_color: "default",
+      url: "default"
+    },
+    GU: {
+      name: "Guam",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      hide: "yes"
+    },
+    VI: {
+      name: "Virgin Islands",
+      image_source: "x.png",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      hide: "yes"
+    },
+    PR: {
+      name: "Puerto Rico",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      hide: "yes"
+    },
+    AS: {
+      name: "American Samoa",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      hide: "yes"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      description: "default",
+      color: "#feea71",
+      hover_color: "default",
+      url: "default",
+      hide: "yes"
+    }
+  },
+  locations: {
+    "0": {
+      name: "New York",
+      lat: "40.71",
+      lng: "-74.0059731",
+      image_url: "default",
+      opacity: "default"
+    },
+    "1": {
+      name: "Anchorage",
+      lat: "61.2180556",
+      lng: "-149.9002778"
+    }
+  },
+  labels: {
+    HI: {
+      color: "default",
+      hover_color: "default",
+      font_family: "default",
+      pill: "yes",
+      width: "default"
+    }
+  },
+  
+};
+
---
+++ assets/js/select.js
@@ -0,0 +1,219 @@
+/* Copyright 2017, Chris Youderian, SimpleMaps, http://simplemaps.com
+ Released under MIT license - https://opensource.org/licenses/MIT 
+ */ 
+ (function(plugin){
+
+  // Rest of the code...
+
+
+  // Start helper functions
+  // IE8 support for index of
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(needle) {
+      for (var i = 0; i < this.length; i++) {
+        if (this[i] === needle) {
+          return i;
+        }
+      }
+      return -1;
+    };
+  }
+  
+  // docReady in pure JavaScript, source: https://github.com/jfriend00/docReady/blob/master/docready.js, MIT
+  (function(funcName, baseObj) {
+    funcName = funcName || "docReady";
+    baseObj = baseObj || window;
+    var readyList = [];
+    var readyFired = false;
+    var readyEventHandlersInstalled = false;
+    
+    function ready() {
+      if (!readyFired) {
+        readyFired = true;
+        for (var i = 0; i < readyList.length; i++) {
+          readyList[i].fn.call(window, readyList[i].ctx);
+        }
+        readyList = [];
+      }
+    }
+    
+    function readyStateChange() {
+      if (document.readyState === "complete") {
+        ready();
+      }
+    }
+    
+    baseObj[funcName] = function(callback, context) {
+      if (readyFired) {
+        setTimeout(function() {
+          callback(context);
+        }, 1);
+        return;
+      } else {
+        readyList.push({
+          fn: callback,
+          ctx: context
+        });
+      }
+      
+      if (document.readyState === "complete" || (!document.attachEvent && document.readyState === "interactive")) {
+        setTimeout(ready, 1);
+      } else if (!readyEventHandlersInstalled) {
+        if (document.addEventListener) {
+          document.addEventListener("DOMContentLoaded", ready, false);
+          window.addEventListener("load", ready, false);
+        } else {
+          document.attachEvent("onreadystatechange", readyStateChange);
+          window.attachEvent("onload", ready);
+        }
+        readyEventHandlersInstalled = true;
+      }
+    };
+  })("docReady", window);
+  // End helper functions
+  
+  window[plugin] = function() {
+    return {
+      map: false,
+      on_shift: false,
+      selected_color: false
+    };
+  }();
+  
+  docReady(function() {
+    var me = window[plugin];
+    var map = me.map ? me.map : simplemaps_usmap; //usmap is default
+    var on_shift = me.on_shift;
+    var selected_color = me.selected_color ? me.selected_color : map.mapdata.main_settings.state_hover_color;
+    var selected = [];
+    var max = me.max ? me.max : false;
+    var original_mapdata = JSON.parse(JSON.stringify(map.mapdata));
+    var main_settings = map.mapdata.main_settings;
+    
+    function check_mapdata(state) {
+      // Make sure a color exists for each state
+      if (!map.mapdata.state_specific[state]) {
+        map.mapdata.state_specific[state] = {};
+      }
+      
+      if (!original_mapdata.state_specific[state]) {
+        original_mapdata.state_specific[state] = {};
+        original_mapdata.state_specific[state].color = 'default';
+      } else if (!original_mapdata.state_specific[state].color) {
+        original_mapdata.state_specific[state].color = 'default';
+      }
+    }
+    
+    var deselect = function(state) {
+      map.states[state].stop(); // Prevents fade time from interfering with deselect
+      var index = selected.indexOf(state);        
+      if (index > -1) { // Deselect state
+        selected.splice(index, 1);   
+        check_mapdata(state);
+        map.mapdata.state_specific[state].color = original_mapdata.state_specific[state].color;
+      }    
+      done(state);
+    };
+    
+    var check_max = function(state) {
+      if (me.max && selected.length >= me.max) {
+        var first = selected[0];
+        me.deselect(first);
+      }      
+    };
+    
+    var select = function(state) {
+      var index = selected.indexOf(state);        
+      if (index < 0) { // Make sure a state is selectable
+        check_mapdata(state);
+        check_max();
+        map.mapdata.state_specific[state].color = me.selected_color;
+        selected.push(state);
+        done(state);
+      }      
+    };
+    
+    var select_all = function() {
+      for (var state in simplemaps_usmap_mapinfo.paths) {
+        select(state);
+      }
+    };
+    
+    var deselect_all = function() {
+      var length = selected.length;
+      for (var i = 1; i < length + 1; i++) {
+        var id = length - i;
+        var state = selected[id];
+        deselect(state);
+      }
+    };    
+    
+    function done(state) {
+      map.refresh_state(state);
+      me.selected = selected; // Update value        
+    }
+  
+    var upon_click = function(state, e) {   
+      var ul = document.getElementById('selected-states-list');
+      var li = document.createElement('li');
+      if (me.on_shift) { // Select on shift+click
+        var evt = e || w.event;
+        var length = me.selected.length;
+        var index = me.selected.indexOf(state);  
+        var last_state = me.selected[length - 1];
+        if (length === 0) {
+          me.select(state);
+        } else if (length > 0) {
+          if (evt.shiftKey) {
+            if (index > -1) {
+              me.deselect(state);
+            } else {
+              me.select(state);
+            }
+          } else { 
+            me.deselect_all(last_state);
+            me.select(state);
+          }
+        }        
+      } else { // Select on click
+        var index = selected.indexOf(state);   
+        console.log(index);
+        if (index > -1) { // Deselect state
+          deselect(state);
+          var listItem = ul.querySelector('li[data-state="' + state + '"]');
+          if (listItem) {
+            ul.removeChild(listItem);
+          }
+        } else { 
+          select(state);
+          li.textContent = state;
+          li.setAttribute('data-state', state);
+          ul.appendChild(li);
+        }
+      }
+    };
+    
+    map.plugin_hooks.click_state.push(upon_click);   
+    
+    window[plugin] = function() {
+      return {
+        // Inputs
+        map: map,
+        on_shift: on_shift,
+        selected_color: selected_color,
+        max: max,
+        // Outputs
+        selected: selected, 
+        // Methods
+        select: select,
+        deselect: deselect,
+        select_all: select_all,
+        deselect_all: deselect_all      
+      };
+    }();
+    
+    me = window[plugin];
+  
+  });
+  
+})('simplemaps_select');
